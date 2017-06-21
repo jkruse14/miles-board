3b3e3c24e4dff910cb65e4c4ac281c157f483aed
Justin Kruse <kruser14@gmail.com>
2017-06-21 13:50:48 -0500

    add configs to app.module, app was failing when in separate files. need to research later

--- app/assets/javascripts/app.auth.config.js
+++ app/assets/javascripts/app.auth.config.js
@@ -1,33 +1,33 @@
-(function () {
-    'use strict';
+// (function () {
+//     'use strict';
 
-    angular
-        .module('milesBoard')
-        .config(authConfig);
+//     angular
+//         .module('milesBoard')
+//         .config(authConfig);
 
-    authConfig.$inject = ['$authProvider'];
+//     authConfig.$inject = ['$authProvider'];
 
-    /** @ngInject */
-    function authConfig($authProvider) {
-        $authProvider.configure(
-            [{
-                'default': {
-                    apiUrl: 'http://localhost:8000',
-                    emailRegistrationPath: '/users',
-                    validateOnPageLoad: true,
-                },
-                'user': {
-                    apiUrl: 'http://localhost:8000',
-                    emailRegistrationPath: '/users',
-                    validateOnPageLoad: true,
-                },
-                'team_owner': {
-                    apiUrl: 'http://localhost:8000',
-                    emailRegistrationPath: '/team_owners',
-                    validateOnPageLoad: true,
-                }
-            }]
-        );
-    }
+//     /** @ngInject */
+//     function authConfig($authProvider) {
+//         $authProvider.configure(
+//             [{
+//                 'default': {
+//                     apiUrl: 'http://localhost:8000',
+//                     emailRegistrationPath: '/users',
+//                     validateOnPageLoad: true,
+//                 },
+//                 'user': {
+//                     apiUrl: 'http://localhost:8000',
+//                     emailRegistrationPath: '/users',
+//                     validateOnPageLoad: true,
+//                 },
+//                 'team_owner': {
+//                     apiUrl: 'http://localhost:8000',
+//                     emailRegistrationPath: '/team_owners',
+//                     validateOnPageLoad: true,
+//                 }
+//             }]
+//         );
+//     }
 
-})();
+// })();
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,62 @@
 (function(){
     'use strict';
 
-    angular.module('milesBoard', [
-        'environment',
-        'restangular',
-        'ui.router',
-        'templates',
-        'ui.bootstrap',
-        'ng-token-auth',
-        'ngStorage'
-    ])
+    angular
+        .module('milesBoard', [
+            'environment',
+            'restangular',
+            'ui.router',
+            'templates',
+            'ui.bootstrap',
+            'ng-token-auth',
+            'ngStorage'
+        ])
+        .config(EnvironmentConfig)
+        .config(authConfig);
+
+    authConfig.$inject = ['$authProvider'];
+
+    /** @ngInject */
+    function authConfig($authProvider) {
+        $authProvider.configure(
+            [{
+                'default': {
+                    apiUrl: 'http://localhost:8000',
+                    emailRegistrationPath: '/users',
+                    validateOnPageLoad: true,
+                },
+                'user': {
+                    apiUrl: 'http://localhost:8000',
+                    emailRegistrationPath: '/users',
+                    validateOnPageLoad: true,
+                },
+                'team_owner': {
+                    apiUrl: 'http://localhost:8000',
+                    emailRegistrationPath: '/team_owners',
+                    validateOnPageLoad: true,
+                }
+            }]
+        );
+    }
+
+    EnvironmentConfig.$inject = ['envServiceProvider'];
+
+    function EnvironmentConfig(envServiceProvider) {
+        envServiceProvider.config({
+            domains: {
+                development: ['localhost'],
+                production: ['miles-board.herokuapp.com']
+            },
+            vars: {
+                development: {
+                    apiUrl: 'http://localhost:8000'
+                },
+                production: {
+                    apiUrl: 'https://miles-board.herokuapp.com'
+                },
+            }
+        })
+
+        envServiceProvider.check();
+    }
 })();
