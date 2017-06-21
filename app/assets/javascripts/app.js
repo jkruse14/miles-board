75f7688d55f4110a8c8a0652315de3e665731a92
Justin Kruse <kruser14@gmail.com>
2017-06-21 15:13:18 -0500

    testing

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,61 +3,61 @@
 
     angular
         .module('milesBoard', [
-            // 'environment',
-            // 'restangular',
-            // 'ui.router',
-            // 'templates',
-            // 'ui.bootstrap',
-            // 'ng-token-auth',
-            // 'ngStorage'
+            'environment',
+            'restangular',
+            'ui.router',
+            'templates',
+            'ui.bootstrap',
+            'ng-token-auth',
+            'ngStorage'
         ])
-    //     .config(EnvironmentConfig)
-    //     .config(authConfig);
+        .config(EnvironmentConfig)
+        .config(authConfig);
 
-    // authConfig.$inject = ['$authProvider'];
+    authConfig.$inject = ['$authProvider'];
 
-    // /** @ngInject */
-    // function authConfig($authProvider) {
-    //     $authProvider.configure(
-    //         [{
-    //             'default': {
-    //                 apiUrl: 'http://localhost:8000',
-    //                 emailRegistrationPath: '/users',
-    //                 validateOnPageLoad: true,
-    //             },
-    //             'user': {
-    //                 apiUrl: 'http://localhost:8000',
-    //                 emailRegistrationPath: '/users',
-    //                 validateOnPageLoad: true,
-    //             },
-    //             'team_owner': {
-    //                 apiUrl: 'http://localhost:8000',
-    //                 emailRegistrationPath: '/team_owners',
-    //                 validateOnPageLoad: true,
-    //             }
-    //         }]
-    //     );
-    // }
+    /** @ngInject */
+    function authConfig($authProvider) {
+        $authProvider.configure(
+            [{
+                'default': {
+                    apiUrl: 'http://localhost:8000',
+                    emailRegistrationPath: '/users',
+                    validateOnPageLoad: true,
+                },
+                'user': {
+                    apiUrl: 'http://localhost:8000',
+                    emailRegistrationPath: '/users',
+                    validateOnPageLoad: true,
+                },
+                'team_owner': {
+                    apiUrl: 'http://localhost:8000',
+                    emailRegistrationPath: '/team_owners',
+                    validateOnPageLoad: true,
+                }
+            }]
+        );
+    }
 
-    // EnvironmentConfig.$inject = ['envServiceProvider'];
+    EnvironmentConfig.$inject = ['envServiceProvider'];
 
-    // /** @ngInject */
-    // function EnvironmentConfig(envServiceProvider) {
-    //     envServiceProvider.config({
-    //         domains: {
-    //             development: ['localhost'],
-    //             production: ['miles-board.herokuapp.com']
-    //         },
-    //         vars: {
-    //             development: {
-    //                 apiUrl: 'http://localhost:8000'
-    //             },
-    //             production: {
-    //                 apiUrl: 'https://miles-board.herokuapp.com'
-    //             },
-    //         }
-    //     })
+    /** @ngInject */
+    function EnvironmentConfig(envServiceProvider) {
+        envServiceProvider.config({
+            domains: {
+                development: ['localhost'],
+                production: ['miles-board.herokuapp.com']
+            },
+            vars: {
+                development: {
+                    apiUrl: 'http://localhost:8000'
+                },
+                production: {
+                    apiUrl: 'https://miles-board.herokuapp.com'
+                },
+            }
+        })
 
-    //     envServiceProvider.check();
-    // }
+        envServiceProvider.check();
+    }
 })();
