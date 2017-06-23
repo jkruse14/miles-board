702ec5e80eb8cab64b5716252ad4e3aa44af5fcf
Justin Kruse <kruser14@gmail.com>
2017-06-23 09:52:57 -0500

    remove unneeded migration

--- db/migrate/20170530172806_add_index_to_users_email.rb
+++
@@ -1,6 +0,0 @@
-# add an index and force uniqueness on email for users
-class AddIndexToUsersEmail < ActiveRecord::Migration[5.0]
-  def change
-    add_index :users, :email, unique: true
-  end
-end
