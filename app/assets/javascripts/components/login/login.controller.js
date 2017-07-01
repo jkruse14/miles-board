374ea2b50752d6ecad047363c43869fe74a25507
Justin Kruse <kruser14@gmail.com>
2017-06-30 21:22:17 -0500

    Revert "move all common code for logins to a factory, add flash messge for failed logins"
    This reverts commit a0bab9d6677627b1e94e2b32d8cea0a2d2bd3205.

--- app/assets/javascripts/components/login/login.controller.js
+++ app/assets/javascripts/components/login/login.controller.js
@@ -6,9 +6,9 @@
         .run()
         .controller('LoginController', LoginController);
     
-    LoginController.$inject = ['$state','$localStorage','$window', 'Flash','LoginFactory'];
+    LoginController.$inject = ['$auth', '$localStorage', '$scope', '$state', '$window', 'Flash', 'InvitationCodesApi', 'Restangular', 'UsersApi'];
 
-    function LoginController($state, $localStorage, $window, Flash, LoginFactory) {
+    function LoginController($auth, $localStorage, $scope, $state, $window, Flash, InvitationCodesApi, Restangular, UsersApi) {
         let vm = this;
 
         let PW_CONF_MSG = {
@@ -27,8 +27,16 @@
         vm.$onInit = onInit;
         vm.setTab = setTab;
         vm.handleSubmitClick = handleSubmitClick;
+        vm.handleLogin = handleLogin;
+        vm.handleSubmitRegistration = handleSubmitRegistration;
+        vm.loginSuccess = loginSuccess;
+        vm.loginFail = loginFail;
+        vm.registrationSuccess = registrationSuccess;
+        vm.registrationFail = registrationFail;
+        vm.resendEmailConfirmation = resendEmailConfirmation;
         vm.resetFocusedField = resetFocusedField;
         vm.setFocusedField = setFocusedField;
+        vm.resetPassword = resetPassword;
         vm.updatePassword = updatePassword;
 
         function onInit(){
@@ -41,34 +49,13 @@
             vm.isOwner = false;
 
             if($state.params['reset_token']) {
-                setTab(4)
+                vm.setTab(4)
                 vm.hideAllTabs = true;
                 let message = "Please complete the below to reset your password"
                 Flash.create('info', message, 0, {container: 'login_flash'}, false);
             }
 
-            vm.focused_field = LoginFactory.resetFocusedField();
-        }
-
-        function handleSubmitClick() {
-            vm.submitting = true;
-            switch (vm.tab) {
-                case 0:
-                    LoginFactory.handleLogin(vm.user_info, loginSuccess, loginFail);
-                    break;
-                case 1:
-                    LoginFactory.handleSubmitRegistration(vm.user_info, vm.isOwner, registrationSuccess, registrationFail);
-                    break;
-                case 2:
-                    LoginFactory.resendEmailConfirmation(vm.user_info);
-                    break;
-                case 3:
-                    LoginFactory.resetPassword(user_info);
-                    break;
-                default:
-                    LoginFactory.handleLogin();
-                    break;
-            }
+            resetFocusedField();
         }
 
         function setTab(index) {
@@ -83,24 +70,31 @@
             };
         }
 
-        function resetUserInfo(email) {
-            email !== undefined ? email : '';
-
-            vm.user_info = {
-                first_name: '',
-                last_name: '',
-                password: '',
-                password_confirmation: '',
-                email: email
-            };
+        function handleSubmitClick() {
+            vm.submitting = true;
+            switch(vm.tab) {
+                case 0:
+                    handleLogin();
+                    break;
+                case 1:
+                    handleSubmitRegistration();
+                    break;
+                case 2:
+                    resendEmailConfirmation();
+                    break;
+                case 3: 
+                    resetPassword(user_info);
+                    break;
+                default:
+                    handleLogin();
+                    break;
+            } 
         }
 
-        function setFocusedField(form, field) {
-            vm.focused_field = LoginFactory.setFocusedField(form, field, vm.focused_field);
-        }
-
-        function resetFocusedField() {
-            vm.focused_field = LoginFactory.resetFocusedField();
+        function handleLogin() {
+            
+            $auth.submitLogin(vm.user_info)
+                .then(vm.loginSuccess, vm.loginFail);
         }
 
         function loginSuccess(resp) {
@@ -112,12 +106,43 @@
             $window.location.reload();
         }
 
-        function updatePassword() {
-            LoginFactory.updatePassword(vm.user_info, loginSuccess, loginFail);
+        function loginFail(resp) {
+
         }
 
-        function loginFail(resp) {
-            Flash.create('danger', 'Login Error: '+resp.errors, 0, {container: 'login_flash'}, true)
+        function handleSubmitRegistration() {
+            Flash.clear();
+            let new_user = {
+                first_name: vm.user_info.first_name,
+                last_name: vm.user_info.last_name,
+                password: vm.user_info.password,
+                password_confirmation: vm.user_info.password_confirmation,
+                email: vm.user_info.email
+            }
+
+            let auth_config = vm.isOwner ? 'team_owner' : 'user';
+            if(vm.isOwner) {
+                InvitationCodesApi.get(vm.user_info.owner_confirmation_code).then(function (response){
+                    response = response.plain();
+                    if(response.email === vm.user_info.email && response.used === false) {
+                        vm.user_info.code_id = response.id;
+                        $auth.submitRegistration(new_user, { config: auth_config })
+                            .then(vm.registrationSuccess, vm.registrationFail);
+                    } else {
+                        let message = 'There was an error with your code: </br>'
+                        if(vm.user_info.email !== response.email) {
+                            message += 'email associated with code does not match above'
+                        } else if( response.used === true) {
+                            message += 'this code was already used'
+                        }
+
+                        Flash.message('warning',message,0,{container:'regform_flash'}, true);
+                    }
+                })
+            } else {
+                $auth.submitRegistration(new_user, {config: auth_config})
+                    .then(vm.registrationSuccess, vm.registrationFail);
+            }
         }
 
         function registrationSuccess(resp) {
@@ -141,5 +166,95 @@
             vm.submitting = false;
         }
 
+        function resendEmailConfirmation() {
+           Restangular.all('users').customGET('resend_confirmation',{email: vm.user_info.email})
+            .then(registrationSuccess(resp),
+                function(resp){
+                    let message = 'Whoops... something went wrong while sending your confirmation email'
+                    Flash.create('danger', message, 0, { container: 'regform_flash' }, true);
+                    vm.registered = true;
+                });
+        }
+
+        function resetUserInfo() {
+            let email = '';
+            if (vm.user_info && vm.user_info.email) {
+                email = vm.user_info.email;
+            }
+            vm.user_info = {
+                first_name: '',
+                last_name: '',
+                password: '',
+                password_confirmation: '',
+                email: email
+            };
+        }
+
+        function setFocusedField(form, field) {
+            vm.focused_field[form][field] = true;
+        }
+
+        function resetFocusedField() {
+            vm.focused_field = {
+                loginForm: {
+                    email: false,
+                    password: false
+                },
+                newMemberForm: {
+                    email: false,
+                    first_name: false,
+                    last_name: false,
+                    password: false,
+                    password_confirmation: false
+                },
+                resendEmailForm: {
+                    email: false
+                },
+                resetPasswordForm: {
+                    email: false
+                },
+                updatePasswordForm: {
+                    email: false,
+                    password: false,
+                    password_confirmation: false                    
+                }
+            };
+        }
+
+        function resetPassword(user) {
+            Flash.clear();
+            let u = {email: user.email}
+            $auth.requestPasswordReset(u)
+                .then(function (resp) {
+                    let message = "Success!<br />An email has been sent to reset your password"
+                    Flash.create('success', message, 0, { container: 'profile_flash' }, true)
+                })
+                .catch(function (resp) {
+                    // handle error response
+                    MilesBoardImages.errorReader(error);
+                });
+        }
+
+        function updatePassword() {
+            Flash.clear();
+            let message = 'Updating...'
+            Flash.create('info', message, 0, {container: 'reset_form'}, true)
+            $auth.updatePassword({ password: vm.user_info.password, 
+                                   password_confirmation: vm.user_info.password_confirmation,
+                                   email: vm.user_info.email,
+                                   reset_password_token: $state.params['reset_token']})
+                .then(function (resp) {
+                    // handle success response
+                    Flash.clear();
+                    $auth.submitLogin(vm.user_info)
+                        .then(vm.loginSuccess, vm.loginFail)
+                })
+                .catch(function (resp) {
+                    // handle error response
+                    Flash.clear();
+                    let message = "Whoops... There seems to have been an error updating your password. <br />Please try again or request a new email to be sent";
+                    Flash.create('danger',message, 0, {container: 'reset_form'}, true)
+                });
+        }
     }
 })();
