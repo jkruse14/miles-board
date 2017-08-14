21bec28b5ba49323aa4dcb4d8252528ee2f370c9
Justin Kruse <kruser14@gmail.com>
2017-08-14 12:24:26 -0500

    prod not building

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module MilesBoard
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.assets.paths << Rails.root.join('node_modules')
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'bootstrap', 'less')
+    config.assets.paths << Rails.root.join('bower_components')
     #config.assets.paths << Rails.root.join('assets', 'stylesheets', 'teams.css.less')
 
     config.assets.precompile << /.*.(?:eot|svg|ttf|woff|woff2|eps|es6)$/
