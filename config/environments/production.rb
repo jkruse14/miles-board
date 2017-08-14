6198d3f0b147d12c520cc976a2555b922ff07d24
Justin Kruse <kruser14@gmail.com>
2017-08-14 12:27:49 -0500

    Revert "prod not building"
    This reverts commit 21bec28b5ba49323aa4dcb4d8252528ee2f370c9.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  #config.assets.paths << Rails.root.join('bower_components')
+  config.assets.paths << Rails.root.join('bower_components')
   config.assets.prefix = "/assets"
   # Code is not reloaded between requests.
   config.cache_classes = true
